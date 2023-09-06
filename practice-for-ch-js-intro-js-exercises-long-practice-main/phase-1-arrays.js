6d9bfc84aa53daeae4ee0ca49b09440ce0af3be2
David <2018dpollack@gmail.com>
2023-09-06 14:30:02 -0700

    Finished Array Uniq MP

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,14 @@
 // Your code here
+
+Array.prototype.uniq = function () {
+    let array = [];
+    for (let i = 0; i < this.length; i++) {
+        let ele = this[i];
+        if (!array.includes(ele)) {
+            array.push(ele)
+        };
+    };
+    return array
+};
+
+console.log([1,2,2,3,3,3].uniq())
