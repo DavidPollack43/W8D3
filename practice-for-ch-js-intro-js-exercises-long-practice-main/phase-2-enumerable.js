b8680766093fea3ea15892d432d923a1c17566b7
David <2018dpollack@gmail.com>
2023-09-06 16:19:19 -0700

    Finished myReduce

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -5,12 +5,12 @@ function call(acc, ele) {
 
 Array.prototype.myEach = function(callback) {
     for (let i = 0; i < this.length; i++) {
-        callback(25, this[i])
+        callback(this[i], i)
     }
     return null
 }
 
-console.log([1,2,3].myEach(call))
+// console.log([1,2,3].myEach(call))
  // This call back will increment the element by 1
 function mapCall(ele) {
     return ele += 1;
@@ -27,19 +27,35 @@ Array.prototype.myMap = function(callback) {
 // console.log([1,2,3].myMap(mapCall))
 // function(acc, el) => acc += el
 
+//This method will do a callback/whatever the client wants on each element of the array and
+//basically combine them, and there is an optoinal initial value.
 Array.prototype.myReduce = function(callback, initialValue = this[0]) {
-    let acc;
-    let startingIndex;
-    if (initialValue = this[0]) {
+
+    let acc; //Tracking the total...or else we can't update anythnig, we can't keep track anything
+    let startingIndex; //Keeping track of where we start based on if they passed in a initial value
+
+    // This block will set up the starting index based on if they passed in a value
+    if (initialValue === this[0]) {
         acc = this[0]
         startingIndex = 1
     } else {
         acc = initialValue
         startingIndex = 0
     }
-    acc = this.myEach(callback(acc, startingIndex))
+
+    //This function will go through the array, and keep track of the acc/update it
+    this.myEach(function(ele, index) {
+        console.log(`The element is ${ele} and the index is ${index}`)
+        if (index >= startingIndex){
+            console.log(`acc before callback = ${acc}`)
+            acc = callback(acc, ele)
+            console.log(`acc after callback = ${acc}`)
+        }
+    })
+    return acc;
 }
 
-[1, 2, 3].myReduce(function(acc, el) {
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
     return acc + el;
-  }, 25);
+  }));
