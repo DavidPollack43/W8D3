020f2b821b09a90a509608286c919b82e1d453da
David <2018dpollack@gmail.com>
2023-09-06 14:57:09 -0700

    Finished transpose

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -25,4 +25,17 @@ Array.prototype.twoSum = function () {
     return array
 }
 
-console.log([-1, 0, 2, -2, 1].twoSum())
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
+Array.prototype.transpose = function () {
+    let array = []
+    for (let i = 0; i < this.length; i++){
+        let row = [];
+        for (let j = 0; j < this.length; j++) {
+            row.push(this[j][i])
+        }
+        array.push(row)
+    }
+    return array
+}
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose()) // => [[0,3,6], [1,4,7], [2,5,8]]
