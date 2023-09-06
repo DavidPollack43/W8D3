333e4cd66f444406285c66ce16913a9b7c530d9e
Dylan <dylanquach@gmail.com>
2023-09-06 15:06:20 -0700

    Add myEach

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,13 @@
 // Your code here
+function call(ele) {
+    console.log(ele)
+}
+
+Array.prototype.myEach = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i])
+    }
+    return null
+}
+
+console.log([1,2,3].myEach(call))
