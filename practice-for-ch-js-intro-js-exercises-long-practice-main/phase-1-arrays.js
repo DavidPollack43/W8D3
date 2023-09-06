d2797ad62f13f4a02290f1c1437d0f61b119d102
Dylan <dylanquach@gmail.com>
2023-09-06 14:42:38 -0700

    Add twoSum

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -11,4 +11,18 @@ Array.prototype.uniq = function () {
     return array
 };
 
-console.log([1,2,2,3,3,3].uniq())
+// console.log([1,2,2,3,3,3].uniq())
+
+Array.prototype.twoSum = function () {
+    let array = [];
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) {
+                array.push([i, j])
+            }
+        }
+    }
+    return array
+}
+
+console.log([-1, 0, 2, -2, 1].twoSum())
