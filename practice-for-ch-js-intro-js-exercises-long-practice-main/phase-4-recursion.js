0aa35fac9900c7860c822f94decd513bec81dd73
Dylan <dylanquach@gmail.com>
2023-09-06 16:48:15 -0700

    Add range

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
@@ -1,1 +1,12 @@
 // Your code here
+function range(start, end) {
+    if (start === end) {
+        return [start]
+    }
+    let arr = (range(start, end - 1))
+    arr.push(end)
+    return arr
+}
+
+console.log(range(3,15))
+// [1]
