bc14add004ad480aebbb0db2a9311af55253c643
Dylan <dylanquach@gmail.com>
2023-09-06 16:28:32 -0700

    Add bubbleSort

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,16 @@
 // Your code here
+Array.prototype.bubbleSort = function() {
+    let sorted = false
+    while (!sorted) {
+        sorted = true
+        for (let i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i+1]) {
+                [this[i], this[i+1]] = [this[i+1], this[i]]
+                sorted = false
+            }
+        }
+    }
+    return this
+}
+
+console.log([9,8,17,5,4].bubbleSort())
