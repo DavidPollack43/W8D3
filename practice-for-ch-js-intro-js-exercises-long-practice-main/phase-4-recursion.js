074c0cfd85bf73dd7297b5b4ff84c2236990e7fd
David <2018dpollack@gmail.com>
2023-09-06 16:57:51 -0700

    Finished sumRec

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
@@ -8,5 +8,16 @@ function range(start, end) {
     return arr
 }
 
-console.log(range(3,15))
+// console.log(range(3,15))
 // [1]
+
+function sumRec(arr) {
+    if (arr.length === 1){
+        return arr[0];
+    };
+    let element = arr.pop();
+    return sumRec(arr) + element;
+};
+
+// console.log(sumRec([1,2,3])) // 6
+
